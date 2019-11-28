4441ea0d161e60100959a04e29f177e743e80d25
Guillermo <rigui@hotmail.es>
2019-11-28 11:26:29 +0100

    Lesson 6
    Cambios para completar lo que se pide en esa lección

--- Shiny_Tutorial/stock-vis/app.R
+++ Shiny_Tutorial/stock-vis/app.R
@@ -44,10 +44,15 @@ server <- function(input, output) {
                to = input$dates[2],
                auto.assign = FALSE)
   })
+  
+  tempInput <- reactive({
+    if (!input$adjust) (return(dataInput()))
+    adjust(dataInput())
+  })
 
   output$plot <- renderPlot({
 
-    chartSeries(dataInput(), theme = chartTheme("white"),
+    chartSeries(tempInput(), theme = chartTheme("white"),
                 type = "line", log.scale = input$log, TA = NULL)
   })
 
