8aee583fad5b2daf85d9777a463cdda21c71ec2d
Angel Igareta <angel@igareta.com>
2019-11-28 21:05:44 +0100

    Format code of lesson 1

--- Shiny_Tutorial/lesson-1/app.R
+++ Shiny_Tutorial/lesson-1/app.R
@@ -1,11 +1,12 @@
-# App for the Lesson 2
+# App for the Lesson 1
 
 library(shiny)
 
 # Define UI ----
 ui <- fluidPage(
     tags$head(tags$style(
-        HTML('
+        HTML(
+            '
         #main-panel {
             color: white;
             background-color: #3f51b5;
@@ -34,47 +35,69 @@ ui <- fluidPage(
 
         body, label, input, button, select {
           font-family: "Arial";
-        }')
+        }'
+        )
     )),
-    id="main-panel",
+    id = "main-panel",
     titlePanel("My Shiny App"),
     sidebarLayout(
         sidebarPanel(
-            id="panel-left",
+            id = "panel-left",
             h2("Installation"),
-            p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
+            p(
+                "Shiny is available on CRAN, so you can install it in the usual way from your R console:"
+            ),
             code('install.packages("shiny")'),
             br(),
             br(),
             br(),
             br(),
-            img(src = "rstudio.png", height = 70, width = 200),
+            img(
+                src = "rstudio.png",
+                height = 70,
+                width = 200
+            ),
             br(),
             "Shiny is a product of ",
             span("RStudio", style = "color:blue")
         ),
         mainPanel(
-            id="panel-right",
+            id = "panel-right",
             h3("Introducing Shiny"),
-            p("Shiny is a new package from RStudio that makes it ",
-              em("incredibly easy "),
-              "to build interactive web applications with R."),
+            p(
+                "Shiny is a new package from RStudio that makes it ",
+                em("incredibly easy "),
+                "to build interactive web applications with R."
+            ),
             br(),
-            p("For an introduction and live examples, visit the ",
-              a("Shiny homepage.",
-                href = "http://shiny.rstudio.com")),
+            p(
+                "For an introduction and live examples, visit the ",
+                a("Shiny homepage.",
+                  href = "http://shiny.rstudio.com")
+            ),
             br(),
             h2("Features"),
-            p("- Build useful web applications with only a few lines of code—no JavaScript required."),
-            p("- Shiny applications are automatically 'live' in the same way that ",
-              strong("spreadsheets"),
-              " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.")
+            p(
+                "- Build useful web applications with only a few lines of code—no JavaScript required."
+            ),
+            p(
+                "- Shiny applications are automatically 'live' in the same way that ",
+                strong("spreadsheets"),
+                " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser."
+            )
         )
     ),
-    mainPanel(id="panel-bottom", "This is what a second main panel would look like if it didn't have the SidebarLayout before it.",
-              p("Im a paragrapgh inside the second main panel.")),
-    p("You can also create separated paragraphs and the rest of elemets outside the function without problems."),
-    p("However paragraphs may look a bit weird if they are not inside other function like 'main panel' (see precious paragraph)")
+    mainPanel(
+        id = "panel-bottom",
+        "This is what a second main panel would look like if it didn't have the SidebarLayout before it.",
+        p("Im a paragrapgh inside the second main panel.")
+    ),
+    p(
+        "You can also create separated paragraphs and the rest of elemets outside the function without problems."
+    ),
+    p(
+        "However paragraphs may look a bit weird if they are not inside other function like 'main panel' (see precious paragraph)"
+    )
 )
 
 # Define server logic ----
